f3a9db81a58f63f26fb8c49299ef53a83e063d52
Mustafa Aqeel <thhfgnb@gmail.com>
2025-11-15 19:41:36 +0300

    InitialCommit

---
+++ TaskManagement.Infrastructure/Extensions/ServiceCollectionExtensios.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+         
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+           
+            services.AddDbContext<TaskDbContext>(options =>
+                options.UseSqlServer(
+                    connectionString,
+                    b => b.MigrationsAssembly(typeof(ServiceCollectionExtensions).Assembly.FullName) 
+                ));
+
+
+            return services;
+        }
+    }
+
---
+++ TaskManagement.Infrastructure/Presistence/TaskDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+internal class TaskDbContext(DbContextOptions<TaskDbContext> options) : DbContext(options)
+{
+    internal DbSet<Task> Tasks { get; set; }
+}
